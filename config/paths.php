20edb32fac1eed1a9d1e42e6a61cd118e35e7228
Lamin Sanneh <lswebapps@gmail.com>
2013-08-23 13:00:14 +0100

    Made path constant to work both on remote and local server

--- config/paths.php
+++ config/paths.php
@@ -1,5 +1,10 @@
 <?php  
 
-define('URL', 'http://localhost/LSMiniPhpMvc/');
+if(strpos($_SERVER['HTTP_HOST'], 'localhost') !== false){
+	define('URL', 'http://localhost/LSMiniPhpMvc/');
+}
+else{
+	//Define url constant here for your remote server
+}
 
 ?>
