d527f9ef865b646c0d3841247feda68597f86dd9
Lamin Sanneh <lswebapps@gmail.com>
2013-08-23 10:17:47 +0100

    Reverted back to revision with hash 2eb1a650d0069b750e925ee9872d9df83ef1eda4

--- config/database.php
+++ config/database.php
@@ -1,24 +1,10 @@
 <?php  
 
-//Loading different database config based on 
-//whether we are on local or remote server
 
-//We are on local machine
-if(strpos('localhost',$_SERVER['SERVER_NAME']) !=== false){
-	define('DB_TYPE', 'mysql');
-	define('DB_HOST', 'localhost');
-	define('DB_NAME', 'LSMiniPhpMvc');
-	define('DB_USER', 'root');
-	define('DB_PASS', '');
-}
-else{
-//We are on remote machine
-	//Put remote machines database config here
-	define('DB_TYPE', 'mysql');
-	define('DB_HOST', 'localhost');
-	define('DB_NAME', 'LSMiniPhpMvc');
-	define('DB_USER', 'root');
-	define('DB_PASS', '');
-}
+define('DB_TYPE', 'mysql');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'LSMiniPhpMvc');
+define('DB_USER', 'root');
+define('DB_PASS', '');
 
 ?>
