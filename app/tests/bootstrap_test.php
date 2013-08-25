9d3eabbcffb55da059f681da04647c336cf0cf44
Lamin Sanneh <lswebapps@gmail.com>
2013-08-25 22:00:50 +0100

    Add test file for bootstrap and tested "extract_url_segment" function in Bootstrap class

---
+++ app/tests/bootstrap_test.php
@@ -0,0 +1,36 @@
+<?php 
+
+require(__DIR__.'/../packages/autoload.php');
+define('APP_PATH', __DIR__.'/../');
+/**
+* 
+*/
+class BootstrapTest extends PHPUnit_Framework_TestCase
+{
+	
+	function testExtract_url_segments(){
+
+		//Doesnt matter whats passed in all controller constructors
+		$bootstrap = new Bootstrap("index");
+		$result = $bootstrap->extract_url_segments("index");
+		$expected = array('index');
+		$this->assertEquals($expected, $result);
+
+		$bootstrap2 = new Bootstrap("index/index");
+		$result2 = $bootstrap2->extract_url_segments("home/index");
+		$expected2 = array('home', 'index');
+		$this->assertEquals($expected, $result);
+
+		$bootstrap3 = new Bootstrap("index/index/2");
+		$result3 = $bootstrap2->extract_url_segments("index/index/2");
+		$expected3 = array('index', 'index', 2);
+		$this->assertEquals($expected, $result);
+
+		$bootstrap4 = new Bootstrap("index/index");
+		$result4 = $bootstrap2->extract_url_segments("");
+		$expected4 = array("index");
+		$this->assertEquals($expected, $result);
+	}
+}
+
+?>
