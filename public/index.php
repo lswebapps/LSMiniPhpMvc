77dbbe7b357442c7714594b9d0fc819703d2f718
Lamin Sanneh <lswebapps@gmail.com>
2013-08-25 21:58:50 +0100

    Added php composer for autoloading packages and files
    Composer was selected to make it easier to include files in our application site-wide without having to make "require" statements anywhere we want those files. Now as we add more file like controllers or models, we don't have to manually load those, composer autoloader handles that for us

--- public/index.php
+++ public/index.php
@@ -3,18 +3,19 @@
 define('APP_PATH', __DIR__.'/../app');
 	
 //use an autoloader
-require(APP_PATH.'/libs/Bootstrap.php');
-require(APP_PATH.'/helpers/controller_helper.php');
-require(APP_PATH.'/libs/Controller.php');
-require(APP_PATH.'/libs/Model.php');
-require(APP_PATH.'/libs/View.php');
-require(APP_PATH.'/libs/Session.php');
-require(APP_PATH.'/libs/Database.php');
+require(APP_PATH.'/packages/autoload.php');
+// require(APP_PATH.'/libs/Bootstrap.php');
+// require(APP_PATH.'/helpers/controller_helper.php');
+// require(APP_PATH.'/libs/Controller.php');
+// require(APP_PATH.'/libs/Model.php');
+// require(APP_PATH.'/libs/View.php');
+// require(APP_PATH.'/libs/Session.php');
+// require(APP_PATH.'/libs/Database.php');
 require(APP_PATH.'/config/paths.php');
 require(APP_PATH.'/config/database.php');
-require(APP_PATH.'/exceptions/view_exception.php');
-require(APP_PATH.'/exceptions/model_exception.php');
+// require(APP_PATH.'/exceptions/view_exception.php');
+// require(APP_PATH.'/exceptions/model_exception.php');
 
-$app = new Bootstrap();
-
+$app = new Bootstrap($_GET['url']);
+$app->start_app();
 ?>	
